18c83c52e9890fa17a962e7d182adef9ed66061b
Koti Vellanki <vellankikoti@gmail.com>
2025-01-01 15:31:55 +0530

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./App.css"; // Corrected to import App.css
+import "./App.css"; // Correctly pointing to the App.css file
 import App from "./App";
 
-ReactDOM.render(
+// Render the App component into the root element
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
